349fe83afb58ad3970e27be0c1e662f15d8c262e
chaozhouzhang <13760289294@139.com>
2020-08-07 06:54:33 +0800

    first commit

---
+++ ConstrictLayout/app/src/main/java/androidstack/constrict/MainActivity.kt
@@ -0,0 +1,11 @@
+package androidstack.constrict
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
